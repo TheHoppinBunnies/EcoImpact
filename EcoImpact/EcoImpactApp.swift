b01b070206977cc0da31f307b27d609e2ce4baa8
TheHoppinBunnies <thehoppinbunnies@gmail.com>
2024-04-04 13:23:10 -0400

    Added starter files

---
+++ EcoImpact/EcoImpactApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoImpactApp.swift
+//  EcoImpact
+//
+//  Created by Othmane EL MARIKY on 2024-04-04.
+//
+
+import SwiftUI
+
+@main
+struct EcoImpactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
