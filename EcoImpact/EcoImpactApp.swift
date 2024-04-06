d4425737260d76cde800cd895f6075650ade0c15
TheHoppinBunnies <thehoppinbunnies@gmail.com>
2024-04-05 22:23:44 -0400

    fdbhdf

--- EcoImpact/EcoImpactApp.swift
+++ EcoImpact/EcoImpactApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct EcoImpactApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
