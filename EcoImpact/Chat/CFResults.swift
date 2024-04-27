4d771dd8e37cf4f625129a224ef7a1fb45641e78
TheHoppinBunnies <157299042+TheHoppinBunnies@users.noreply.github.com>
2024-04-27 10:23:02 -0400

    Update CFResults.swift

--- EcoImpact/Chat/CFResults.swift
+++ EcoImpact/Chat/CFResults.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct CFResults: View {
     @AppStorage("emission") var emission = 0.0
     var body: some View {
-        Text("\(String(format: "%0.2f", emission)) kg of CO2")
-            .font(.title).bold()
-        
-        Text("Average: 16 000 kg of CO2")
+        VStack {
+            Text("\(String(format: "%0.2f", emission)) kg of CO2")
+                .font(.title).bold()
+            
+            Text("Average: 16 000 kg of CO2")
+        }
     }
 }
 
